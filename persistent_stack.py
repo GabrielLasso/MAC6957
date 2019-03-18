c8322d107145498bdd0a296169dc6b016ff1f676
Gabriel Lasso <gabriel.lasso@usp.br>
2019-03-18 17:30:08 -0300

    Create parser for stack

--- persistent_stack.py
+++ persistent_stack.py
@@ -1,3 +1,5 @@
+import sys
+
 class StackCell:
     def __init__(self, value, size, previous, _jmp):
         self.value = value
@@ -41,23 +43,29 @@ def Kth(p, k):
     return p.value
 
 def main():
-    p0 = Stack()
-    p1 = Push(p0,1)
-    p2 = Push(p1,2)
-    p3 = Push(p2,3)
-    p4 = Push(p1,4)
-    print(Top(p2))
-    p5 = Pop(p2)
-    print(Top(p5))
-    p6 = Push(p2,5)
-    print(Top(p4))
-    p7 = Push(p4,6)
-    print(Size(p6))
-    p8 = Push(p3,7)
-    p9 = Pop(p3)
-    print(Top(p7))
-    print(p8)
-    print(p9)
+    versions = []
+    line = sys.stdin.readline()
+    while (line):
+        tokens = line.replace('(', ',').replace(')', ',').split(',')
+        tokens = list(filter(lambda tk: bool(tk), map(lambda tk: tk.strip(), tokens)))
+        if (tokens):
+            functionName = tokens[0]
+            args = list(map(lambda arg: int(arg), tokens[1:]))
+            if (functionName == "Stack"):
+                versions.append(Stack())
+            elif (functionName == "Push"):
+                versions.append(Push(versions[args[0]],args[1]))
+            elif (functionName == "Pop"):
+                versions.append(Pop(versions[args[0]]))
+            elif (functionName == "Size"):
+                print(Size(versions[args[0]]))
+            elif (functionName == "Top"):
+                print(Top(versions[args[0]]))
+            elif (functionName == "Kth"):
+                print(Kth(versions[args[0]], args[1]))
+            elif (functionName == "Print"):
+                print(versions[args[0]])
+        line = sys.stdin.readline()
 
 if __name__ == '__main__':
     main()
